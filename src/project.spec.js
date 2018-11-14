437606205592e8515e9c33e9fc5354d93a22b5b0
artmadeit <this.arthur.email@gmail.com>
2018-11-13 20:38:41 -0500

    refactor: move every spec concerned to sesion to it's own file

--- src/project.spec.js
+++ src/project.spec.js
@@ -31,31 +31,3 @@ describe("add a members", () => {
     );
   });
 });
-
-describe("start new session", () => {
-  let project;
-  beforeEach(() => {
-    project = new Project("ouracademy");
-    project.join(artmadeit);
-    project.join(qpdiam);
-  });
-
-  it("should start a new session with current date", () => {
-    const constantDate = new Date("2017-06-13T04:41:20");
-
-    /*eslint no-global-assign:off*/
-    Date = class extends Date {
-      constructor() {
-        return constantDate;
-      }
-    };
-
-    expect(project.startNewSession().createdAt).toEqual(constantDate);
-  });
-  it("shouldn't create a session when the current session doesn't finished", () => {
-    project.startNewSession();
-    expect(() => project.startNewSession()).toThrow(
-      "There's already a current that doesn't finished"
-    );
-  });
-});
--- src/session.spec.js
+++ src/session.spec.js
@@ -3,36 +3,49 @@
 import { Project } from "./project";
 import { artmadeit, qpdiam } from "./project.spec";
 
-describe("end session", () => {
-  let session;
+describe("session", () => {
+  let project, session;
+  const mockedCurrentDate = new Date("2017-06-13T04:41:20");
 
   beforeEach(() => {
-    const project = new Project("ouracademy");
-    session = project.startNewSession();
-    project.join(artmadeit);
-    project.join(qpdiam);
-    session.end();
-  });
-
-  it("should set end date & finish the session", () => {
-    const constantDate = new Date("2017-06-13T04:41:20");
-
     /*eslint no-global-assign:off*/
     Date = class extends Date {
       constructor() {
-        return constantDate;
+        return mockedCurrentDate;
       }
     };
 
-    expect(session.endDate).toBe(constantDate);
-    expect(session.isFinished).toBeTruthy();
+    project = new Project("ouracademy");
+    session = project.startNewSession();
+    project.join(artmadeit);
+    project.join(qpdiam);
   });
 
-  it("should add all current project members", () => {
-    expect(session.members).toEqual([artmadeit, qpdiam]);
+  describe("start new session", () => {
+    it("should start a new session with current date", () => {
+      expect(session.createdAt).toEqual(mockedCurrentDate);
+    });
+    it("shouldn't create a session when the current session doesn't finished", () => {
+      expect(() => project.startNewSession()).toThrow(
+        "There's already a current that doesn't finished"
+      );
+    });
   });
 
-  describe("voting", () => {
+  describe("ending session", () => {
+    beforeEach(() => {
+      session.end();
+    });
+
+    it("should set end date & finish the session", () => {
+      expect(session.endDate).toBe(mockedCurrentDate);
+      expect(session.isFinished).toBeTruthy();
+    });
+
+    it("should add all current project members", () => {
+      expect(session.members).toEqual([artmadeit, qpdiam]);
+    });
+
     it("no one can vote when the session it's already finished", () => {
       expect(() => session.vote(artmadeit, 11)).toThrow(
         "Session is finished, no one can vote"
