88fbcb7915b61bd0198836c39305a57108b135be
qpdiam <qpdiam@gmail.com>
2018-11-15 21:15:28 -0500

    fix: generate key unique with date

--- src/util.js
+++ src/util.js
@@ -2,5 +2,5 @@ const uuidv5 = require("uuid/v5");
 
 export function generateKey(nameProject) {
   const MY_NAMESPACE = "1b671a64-40d5-491e-99b0-da01ff1f3341";
-  return uuidv5(nameProject, MY_NAMESPACE);
+  return uuidv5(nameProject, MY_NAMESPACE) + Date.now();
 }
